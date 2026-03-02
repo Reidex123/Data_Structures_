1cd0a2c0eaec6da68b69fbe028da50d7a29f3a38
Koketso <koketsogaowelwe2@gmail.com>
2026-03-02 10:55:06 +0200

    "implemented add methods"

--- src/main/java/com/Linked_Lists/doublyLinkedList/doublyLinkedList.java
+++ src/main/java/com/Linked_Lists/doublyLinkedList/doublyLinkedList.java
@@ -54,6 +54,76 @@ public class doublyLinkedList<T> implements Iterable<T> {
         this.size = 0;
     }
 
+    public void add(T data) {
+        if (isEmpty()) {
+            this.head = new Node<>(data);
+            this.size++;
+            Runtime.getRuntime().exit(0);
+        }
+
+        Node<T> current = this.head;
+        Node<T> node = new Node<>(data);
+
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = node;
+        node.prev = current;
+        this.size++;
+    }
+
+    public void addFirst(T data) {
+        if (isEmpty()) {
+            this.head = new Node<>(data);
+            this.size++;
+            Runtime.getRuntime().exit(0);
+        }
+
+        Node<T> node = new Node<>(data);
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+        this.size++;
+    }
+
+    public void addLast(T data) {
+        this.add(data);
+    }
+
+    public void add(int index, T data) {
+        if (index < 0 || index >= this.size) {
+            System.out.println("Index out of bounds!!");
+            Runtime.getRuntime().exit(0);
+        }
+
+        if (isEmpty() || index == 0) {
+            this.addFirst(data);
+            Runtime.getRuntime().exit(0);
+        }
+
+        if (index == this.size - 1) {
+            this.addLast(data);
+            Runtime.getRuntime().exit(0);
+        }
+
+        Node<T> node = new Node<>(data);
+        Node<T> current = this.head;
+
+        for (int i = 0; i <= index; i++) {
+            current = current.next;
+        }
+
+        current.prev.next = node;
+        node.prev = current.prev;
+        node.next = current;
+        current.prev = node;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new listIterator(this);
