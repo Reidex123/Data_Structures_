ee2dd6dbe13eed3719aa2dbc893a038e0c4eaf0a
Koketso <koketsogaowelwe2@gmail.com>
2026-03-30 18:38:25 +0200

    implementation of BFS

--- src/main/java/com/BFS/graph.java
+++ src/main/java/com/BFS/graph.java
@@ -1,5 +1,34 @@
 package com.BFS;
 
 public class graph {
-    
+
+    public java.util.List<vertex> vertices;
+    public static final int INFINITY = Integer.MAX_VALUE;
+
+    public void bfs(vertex s) {
+
+        for (vertex v : vertices) {
+            v.dist = INFINITY;
+        }
+
+        s.dist = 0;
+        s.path = null;
+
+        java.util.Queue<vertex> q = new java.util.LinkedList<>();
+        q.add(s);
+
+        while (!q.isEmpty()) {
+
+            vertex v = q.remove();
+
+            for (vertex w : v.adj) {
+
+                if (w.dist == INFINITY) {
+                    w.dist = v.dist + 1;
+                    w.path = v;
+                    q.add(w);
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/BFS/vertex.java
+++ src/main/java/com/BFS/vertex.java
@@ -1,5 +1,17 @@
 package com.BFS;
 
 public class vertex {
-    
+
+    public String name;
+    public java.util.List<vertex> adj;
+    public int dist;
+    public vertex path;
+
+    public vertex(String name) {
+        
+        this.name = name;
+        adj = new java.util.LinkedList<>();
+        this.dist = graph.INFINITY;
+        path = null;
+    }
 }
